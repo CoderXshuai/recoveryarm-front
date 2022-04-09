fc18544f2c230db5eb830a45089f89f3650b6b6b
coderxshuai <xshuai1024@foxmail.com>
2022-04-09 19:50:30 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <bone></bone>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bone from './components/bone2.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Bone
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
